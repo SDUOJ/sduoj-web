afe0cda89d0681c09c27340be51601b78c06c709
jeshrz <jeshrz@foxmail.com>
2021-06-15 16:59:53 +0800

    feat: add function template

--- src/components/editor/CodeEditor.vue
+++ src/components/editor/CodeEditor.vue
@@ -9,130 +9,80 @@
  -->
 
 <template>
-  <div>
-    <div class="utils clearfix">
-      <div class="lang">
-        <span>Judge Templates: </span>
-        <Select @on-change="onJudgeTemplateChange" :value="judgeTemplate.id" class="adjust">
-          <template v-for="template in judgeTemplateSet">
-            <Tooltip v-if="template.comment.trim() !== ''" :key="template.id" :content="template.comment" style="width: 100%"  placement="right" transfer>
-              <Option :value="template.id" :label="template.title">
-                <span>{{ template.title }}</span>
-                <span style="float: right; color: #ccc">{{ template.type | judgeTemplateTypeName }}</span>
-              </Option>
-            </Tooltip>
-            <Option v-else :value="template.id" :key="template.id" :label="template.title">
-              <span>{{ template.title }}</span>
-              <span style="float: right; color: #ccc">{{ template.type | judgeTemplateTypeName }}</span>
-            </Option>
-          </template>
-        </Select>
-      </div>
-      <div class="upload">
-        <Upload
-          style="display: inline"
-          :file-list.sync="fileList"
-          :show-upload-list="false"
-          :format="['zip']"
-          accept=".zip">
-          <!--:format="judgeTemplate.acceptFileExtensions || []"
-          :accept="(judgeTemplate.acceptFileExtensions || []).map(o => `.${o}`).join(',')">-->
-          <div v-if="uploadFile">
-
-            <Button icon="ios-cloud-upload-outline" v-if="fileList.length === 0">
-              Upload
-            </Button>
-            <Button icon="ios-cloud-upload-outline" v-else>{{ fileList[0].name }}</Button>
-          </div>
-        </Upload>
-        <Button icon="md-close" v-if="fileList.length > 0" @click="clearFiles"/>
-      </div>
-    </div>
-    <div v-if="!this.uploadFile" class="editor">
-      <codemirror :value="code" :options="options" @change="onEditorCodeChange" ref="myEditor"></codemirror>
-    </div>
+  <div :class="{ 'limit': maxHeight }">
+    <textarea ref="editor" class="codeEditor" />
   </div>
 </template>
+
 <script>
-import { codemirror } from 'vue-codemirror-lite'
-import 'codemirror/addon/selection/active-line.js'
-import { JUDGE_TEMPLATE_PROPERITY, JUDGE_TEMPLATE_TYPE } from '_u/constants';
-import Upload from '_c/upload/upload';
+import CodeMirror from 'codemirror'
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/sql/sql';
+import 'codemirror/mode/python/python';
+
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/selection/active-line';
+import 'codemirror/addon/edit/closebrackets';
 
 export default {
-  name: 'CodeMirror',
-  components: {
-    codemirror,
-    Upload
-  },
   props: {
-    code: {
-      type: String,
-      default: ''
-    },
-    file: {
-      type: File,
-      default: null
-    },
-    judgeTemplate: {
-      type: Object,
-      default: () => {}
-    },
-    judgeTemplateSet: {
-      type: Array,
-      default: () => []
+    code: String,
+    mode: String,
+    maxHeight: Boolean,
+    readOnly: {
+      type: Boolean,
+      default: false
     }
   },
-  filters: {
-    judgeTemplateTypeName: type => JUDGE_TEMPLATE_PROPERITY[type].name
-  },
-  data: function () {
+  data: function() {
     return {
-      options: {
-        // codemirror options
+      editor: null,
+      cmOptions: {
+        cursorHeight: 1,
         tabSize: 2,
-        theme: 'default',
-        lineNumbers: true,
-        line: true,
-        // 选中文本自动高亮，及高亮方式
-        matchBrackets: true,
-        styleSelectedText: true,
-        lineWrapping: true,
-        highlightSelectionMatches: { showToken: /\w/, annotateScrollbar: true }
-      },
-      mode: {},
-      fileList: []
-    }
-  },
-  mounted: function() {
-  },
-  methods: {
-    clearFiles: function () {
-      this.fileList = [];
-    },
-    onEditorCodeChange: function(newCode) {
-      this.$emit('update:code', newCode);
-    },
-    onJudgeTemplateChange: function(newVal) {
-      for (let i = 0; i < this.judgeTemplateSet.length; ++i) {
-        if (this.judgeTemplateSet[i].id === newVal) {
-          this.$emit('update:judgeTemplate', this.judgeTemplateSet[i]);
-        }
+        smartIndent: true,        // 是否智能缩进
+        styleActiveLine: true,    // 当前行高亮
+        lineNumbers: true,        // 显示行号
+        lineWrapping: false,      // 自动换行
+        matchBrackets: true,      // 括号匹配显示
+        autoCloseBrackets: true,  // 输入和退格时成对
+        autoRefresh: true         // 自动刷新
       }
     }
   },
-  computed: {
-    uploadFile: function () {
-      return this.judgeTemplate.type === JUDGE_TEMPLATE_TYPE.ADVANCED &&
-        this.judgeTemplate.acceptFileExtensions.includes('zip');
+  methods: {
+    initialize: function () {
+      this.editor = CodeMirror.fromTextArea(this.$refs.editor, this.cmOptions);
+      this.editor.setValue(this.code || '');
+      this.editor.setOption('mode', this.mode);
+      if (this.readOnly) {
+        this.editor.setOption('readOnly', this.readOnly);
+        this.editor.setOption('styleActiveLine', false);
+      }
+      this.editor.refresh();
+      this.editor.on('change', cm => {
+        this.$emit && this.$emit('update:code', cm.getValue());
+      });
+    },
+    refresh: function () {
+      this.$nextTick && this.$nextTick(() => {
+        this.editor && this.editor.refresh();
+      });
     }
   },
+  mounted: function() {
+    this.cmOptions.mode = this.mode;
+    this.initialize();
+  },
   watch: {
-    fileList: function() {
-      if (this.fileList.length > 0) {
-        this.$emit('update:file', this.fileList[0].file);
-      } else {
-        this.$emit('update:file', null);
+    mode: function (mode) {
+      this.editor && this.editor.setOption('mode', mode);
+    },
+    code: function (val) {
+      const editorValue = this.editor.getValue();
+      if (val !== editorValue) {
+        this.editor.setValue(val);
       }
     }
   }
@@ -140,24 +90,19 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.utils {
-  margin-right: -15px;
-  .lang {
-    float: left;
-    padding: 10px 10px 5px 10px;
-    .adjust {
-      width: 300px;
-      margin-left: 10px;
-    }
-  }
-  .upload {
-    float: right;
-    padding: 10px 10px 5px 0;
-  }
+.limit /deep/ .CodeMirror {
+  height: auto;
+  max-height: 200px;
 }
-.editor {
-  margin-right: -15px;
-  border-top: 1px solid #d4d4d5;
-  border-bottom: 1px solid #d4d4d5;
+
+.limit /deep/ .CodeMirror-scroll {
+  height: auto;
+  max-height: 200px;
 }
+
+/deep/ .CodeMirror {
+  font-size: 14px;
+  line-height: 1.5;
+}
+
 </style>
