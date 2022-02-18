41d48d96a87c29dc8fbd5873471874308269454e
Teng Zhang <zhangt2333@gmail.com>
2022-02-18 19:55:56 +0800

    feat: remove some data in overview

--- src/views/contest/ContestOverviewView.vue
+++ src/views/contest/ContestOverviewView.vue
@@ -42,15 +42,15 @@
                 }"
                 style="text-decoration: none; color: black">{{ row.problemTitle }}</router-link>
             </template>
-<!--            <template slot-scope="{ row }" slot="ratio">-->
-<!--              <span>{{ `${row.acceptNum || '-'} / ${row.submitNum || '-'}` }}</span>-->
-<!--            </template>-->
-<!--            <template slot-scope="{ row }" slot="status">-->
-<!--              <JudgeResult :result="row.judgeResult" />-->
-<!--            </template>-->
-<!--            <template slot-scope="{ row }" slot="score">-->
-<!--              <span v-if="row.judgeResult && showJudgeScore">{{ row.judgeScore }}</span>-->
-<!--            </template>-->
+            <template slot-scope="{ row }" slot="ratio">
+              <span>{{ `${row.acceptNum || '-'} / ${row.submitNum || '-'}` }}</span>
+            </template>
+            <template slot-scope="{ row }" slot="status">
+              <JudgeResult :result="row.judgeResult" />
+            </template>
+            <template slot-scope="{ row }" slot="score">
+              <span v-if="row.judgeResult && showJudgeScore">{{ row.judgeScore }}</span>
+            </template>
           </Table>
         </Card>
       </div>
@@ -82,10 +82,10 @@ export default {
       },
       problemColumn: [
         { key: 'problemCode', maxWidth: 60, slot: 'problemCode' },
-        { key: 'problemTitle', title: 'Title', slot: 'title' },
-        { title: 'AC / Submits', slot: 'ratio' },
-        { title: 'Status', slot: 'status' },
-        { title: 'Score', slot: 'score' }
+        { key: 'problemTitle', title: 'Title', slot: 'title' }
+        // { title: 'AC / Submits', slot: 'ratio' },
+        // { title: 'Status', slot: 'status' },
+        // { title: 'Score', slot: 'score' }
       ]
     }
   },
