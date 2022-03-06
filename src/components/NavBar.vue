ded99fee3973f031ee8d5580e2ab0141c0c1e47f
Teng Zhang <zhangt2333@gmail.com>
2022-03-06 15:36:13 +0800

    feat: add NJU logo and text

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -12,9 +12,9 @@
   <div class="header fixed">
     <div class="container">
       <Menu mode="horizontal" theme="light" :active-name="$route.path.split('/')[1]">
-<!--        <div class="logo" @click="$router.push('/')">-->
-<!--          <img src="../assets/logo.png" style="width: 100px; height: 30px;"/>-->
-<!--        </div>-->
+        <div class="logo" @click="$router.push('/')">
+          <img src="../../public/nju-logo.png" style="width: 30px; height: 30px;"/>
+        </div>
 <!--        <MenuItem name="home" :to="{ name: 'home' }">-->
 <!--          <span class="span__menu">Home</span>-->
 <!--        </MenuItem>-->
@@ -22,7 +22,7 @@
 <!--          <span class="span__menu">Problem</span>-->
 <!--        </MenuItem>-->
         <MenuItem name="contest" :to="{ name: 'contest' }">
-          <span class="span__menu">Compiler</span>
+          <span class="span__menu">南京大学《编译原理》课程实验平台</span>
         </MenuItem>
 <!--        <MenuItem name="submission" :to="{ name: 'submission' }">-->
 <!--          <span class="span__menu">Submission</span>-->
